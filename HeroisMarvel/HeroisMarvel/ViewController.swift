61f59746503d9cffbf874ea7b26964ca20d61877
vloss <viniciusvianaloss@gmail.com>
2023-02-28 01:02:40 -0300

    Inclusão de projeto baixado do curso.

---
+++ HeroisMarvel/HeroisMarvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeroisMarvel
+//
+//  Created by Eric Brito on 22/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: true)
+    }
+
+}
+
