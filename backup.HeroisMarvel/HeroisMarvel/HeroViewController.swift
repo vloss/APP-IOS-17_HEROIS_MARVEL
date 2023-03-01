526f14ba66ca348a4f2ac29c8f5065e1352a650a
vloss <viniciusvianaloss@gmail.com>
2023-03-01 12:02:59 -0300

    Backup ambiente de teste para instalacão de PODs

---
+++ backup.HeroisMarvel/HeroisMarvel/HeroViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HeroViewController.swift
+//  HeroisMarvel
+//
+//  Created by Eric Brito on 22/10/17.
+//  Copyright © 2017 Eric Brito. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class HeroViewController: UIViewController {
+
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
